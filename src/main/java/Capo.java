4fa936f894aae88c0039d2fa1cc8527b36305ed5
jupojuji <160387850+justinpoonjj@users.noreply.github.com>
2026-01-28 15:31:18 +0800

    Level 0: Rename chatbot and add greetings

---
+++ src/main/java/Capo.java
@@ -0,0 +1,6 @@
+public class Capo {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm CAPO\nWhat can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
