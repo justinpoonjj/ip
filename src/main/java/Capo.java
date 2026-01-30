aee2de0183793bc8c2263e5df1ddabd2ed468d2d
jupojuji <160387850+justinpoonjj@users.noreply.github.com>
2026-01-30 11:59:40 +0800

    Level 1: echos commands entered by user and exit when typed 'bye'

--- src/main/java/Capo.java
+++ src/main/java/Capo.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Capo {
     public static void main(String[] args) {
+        String userInput;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("_____________________________________________________________");
         System.out.println("Hello! I'm CAPO\nWhat can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_____________________________________________________________");
+        do {
+            userInput = input.nextLine();
+            System.out.println("_____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("_____________________________________________________________");
+        }
+        while(!userInput.equals("bye"));
+        System.out.println("Buh bye. Hope to see you again soon!");
     }
 }
