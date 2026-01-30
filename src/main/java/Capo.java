61ab8fc55ba9f19a503a139117bd9291182b3216
jupojuji <160387850+justinpoonjj@users.noreply.github.com>
2026-01-30 13:41:57 +0800

    Level 2: add the ability to store text entered by user and modified Echo code from do-while loop to a while loop due to the discovery of some bugs

--- src/main/java/Capo.java
+++ src/main/java/Capo.java
@@ -1,20 +1,40 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Capo {
     public static void main(String[] args) {
         String userInput;
+        int index = 0;
+        String[] list = new String[100];
         Scanner input = new Scanner(System.in);
 
         System.out.println("_____________________________________________________________");
         System.out.println("Hello! I'm CAPO\nWhat can I do for you?\n");
         System.out.println("_____________________________________________________________");
-        do {
+        userInput = input.nextLine();
+        while (true) {
+            if (userInput.equals("bye")) {
+                break;
+            }
+            else if (userInput.equals("list")) {
+                System.out.println("_____________________________________________________________");
+                for (int i = 0; i < index; i++) {
+                    System.out.println(i + ". " + list[i]);
+
+                }
+                System.out.println("_____________________________________________________________");
+            }
+            else {
+                System.out.println("_____________________________________________________________");
+                System.out.println(userInput);
+                System.out.println("_____________________________________________________________");
+                list[index] = userInput;
+                index++;
+            }
             userInput = input.nextLine();
-            System.out.println("_____________________________________________________________");
-            System.out.println(userInput);
-            System.out.println("_____________________________________________________________");
         }
-        while(!userInput.equals("bye"));
+        System.out.println("_____________________________________________________________");
         System.out.println("Buh bye. Hope to see you again soon!");
+        System.out.println("_____________________________________________________________");
     }
 }
