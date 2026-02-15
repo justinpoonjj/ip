0cd794075bc347fa295c1b07239cc5e34b155088
jupojuji <160387850+justinpoonjj@users.noreply.github.com>
2026-02-15 16:11:09 +0800

    A-Exceptions: Handled error for potential error in deadline and event tasks

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,7 @@
 public class Parser {
     public static Commands parse (String userInput) throws CapoException {
         String[] split = userInput.split(" ");
-        String keyword = split[0];
+        String keyword = split[0].toLowerCase();
         Task t;
 
         switch(keyword) {
@@ -17,14 +17,20 @@ public class Parser {
             return new UnmarkCommand(unmarkIndex);
         case "todo":
             if (split.length < 2) {
-                throw new CapoException("The description of todo task cannot be empty");
+                throw new CapoException("Please follow the format\n todo [task name]");
             }
             t = new Todo(userInput.substring(keyword.length() + 1));
             return new AddCommand(t);
         case "deadline":
+            if (split.length < 2 || !userInput.contains("/by")) {
+                throw new CapoException("Please follow the format\n deadline [task name] /by [date]");
+            }
             t = new Deadline(userInput.substring(keyword.length() + 1));
             return new AddCommand(t);
         case "event":
+            if (split.length < 2 || !userInput.contains("/from") || !userInput.contains("/to")) {
+                throw new CapoException("Please follow the format\n event [task name] /from [time] /to [time]");
+            }
             t = new Event(userInput.substring(keyword.length() + 1));
             return new AddCommand(t);
         default:
